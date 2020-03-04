d68646a835329bec3902a7aaaf027cb312226533
Hesham <heshamoov90@gmail.com>
2020-03-04 20:59:27 +0400

    Release 4.0
    Signed-off-by: Hesham <heshamoov90@gmail.com>

---
+++ online_lesson.php
@@ -0,0 +1,4 @@
+<h1>Online Lesson</h1>
+<br>
+<div id="otEmbedContainer" style="width:800px; height:640px"></div>
+<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=9be90b93-f053-4ee9-b14b-92e971711a21&room=DEFAULT_ROOM"></script>
